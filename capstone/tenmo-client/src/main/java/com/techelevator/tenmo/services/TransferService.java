83ed5423e3aadfccb0aa8913d1a8bf1ae9cff0a0
Jake Hall <JakeHall1218@yahoo.com>
2022-04-29 09:33:09 -0400

    fixing merge conflict

--- capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -6,10 +6,6 @@ import com.techelevator.tenmo.model.User;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-<<<<<<< HEAD
-import org.springframework.http.MediaType;
-=======
->>>>>>> f52de27e0fa660ba2afab6e6924fa100067a641d
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
@@ -25,7 +21,6 @@ public class TransferService {
         this.currentUser = currentUser;
         baseUrl = url;
     }
-<<<<<<< HEAD
     public void setAuthToken(String authToken) {
         this.authToken = authToken;
     }
@@ -37,16 +32,6 @@ public class TransferService {
         try {
             Scanner scanner = new Scanner(System.in);
             users = restTemplate.exchange(baseUrl + "account/listUsers", HttpMethod.GET, makeAuthEntity(), User[].class).getBody();
-=======
-
-    public void sendTransfer() {
-        User[] users = null;
-        Transfer transfers = new Transfer();
-
-        try {
-            Scanner scanner = new Scanner(System.in);
-            users = restTemplate.exchange(baseUrl + "listUsers", HttpMethod.GET, makeAuthEntity(), User[].class).getBody();
->>>>>>> f52de27e0fa660ba2afab6e6924fa100067a641d
             System.out.println("------------------------------\n"
                     + "Users\n" + "ID\tName\n" +
                     "------------------------------");
@@ -57,7 +42,6 @@ public class TransferService {
             }
             System.out.println("-----------------------------\n"
                     + "Enter Id of user you are sending to (0 to cancel): ");
-<<<<<<< HEAD
             transfer.setAccountTo(Integer.parseInt(scanner.nextLine()));
             transfer.setAccountFrom(currentUser.getUser().getId());
             if (transfer.getAccountTo() != 0) {
@@ -76,25 +60,6 @@ public class TransferService {
 
     }
 
-=======
-            transfers.setAccountTo(Integer.parseInt(scanner.nextLine()));
-            transfers.setAccountFrom(currentUser.getUser().getId());
-            if (transfers.getAccountTo() != 0) {
-                System.out.println("Enter amount: ");
-                try {
-                    transfers.setAmount(new BigDecimal(Double.parseDouble(scanner.nextLine())));
-                } catch (NumberFormatException e) {
-                    System.out.println("Error when entering amount: ");
-                }
-                String amount = restTemplate.exchange(baseUrl + "transfer", HttpMethod.POST, makeAuthEntity(), String.class).getBody();
-                System.out.println(amount);
-            }
-        } catch (Exception e) {
-            System.out.println("");
-        }
-
-    }
->>>>>>> f52de27e0fa660ba2afab6e6924fa100067a641d
     private HttpEntity makeAuthEntity() {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(authToken);
